31fbb06856916730178864c9ef451b4d8077909d
Rohith2202 <82767839+Rohith2202@users.noreply.github.com>
2022-05-30 21:26:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,56 @@
 from playsound import playsound
 from pynput import keyboard
 import msvcrt
-
+"""Change the Path to wherever the note directory or to wherever all the keys are stored"""
 def piano(notess):
     if(notess=="q"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key01.mp3')
+        playsound('/note/key01.mp3')
     if (notess == "w"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key02.mp3')
+        playsound('/note/key02.mp3')
     if (notess == "e"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key03.mp3')
+        playsound('/note/key03.mp3')
     if (notess == "r"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key04.mp3')
+        playsound('/note/key04.mp3')
     if (notess == "t"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key05.mp3')
+        playsound('/note/key05.mp3')
     if (notess == "y"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key06.mp3')
+        playsound('/note/key06.mp3')
     if (notess == "u"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key07.mp3')
+        playsound('/note/key07.mp3')
     if (notess == "i"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key08.mp3')
+        playsound('/note/key08.mp3')
     if (notess == "p"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key09.mp3')
+        playsound('/note/key09.mp3')
     if (notess == "a"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key10.mp3')
+        playsound('/note/key10.mp3')
     if (notess == "s"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key11.mp3')
+        playsound('/note/key11.mp3')
     if (notess == "d"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key12.mp3')
+        playsound('/note/key12.mp3')
     if (notess == "f"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key13.mp3')
+        playsound('/note/key13.mp3')
     if (notess == "g"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key14.mp3')
+        playsound('/note/key14.mp3')
     if (notess == "h"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key15.mp3')
+        playsound('/note/key15.mp3')
     if (notess == "j"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key16.mp3')
+        playsound('/note/key16.mp3')
     if (notess == "k"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key17.mp3')
+        playsound('/note/key17.mp3')
     if (notess == "l"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key18.mp3')
+        playsound('/note/key18.mp3')
     if (notess == "z"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key19.mp3')
+        playsound('/note/key19.mp3')
     if (notess == "x"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key20.mp3')
+        playsound('/note/key20.mp3')
     if (notess == "c"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key21.mp3')
+        playsound('/note/key21.mp3')
     if (notess == "v"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key22.mp3')
+        playsound('/note/key22.mp3')
     if (notess == "b"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key23.mp3')
+        playsound('/note/key23.mp3')
     if (notess == "n"):
-        playsound('C:/Users/ROHITH/Desktop/PianoSound/note/key24.mp3')
+        playsound('/note/key24.mp3')
 
 while True:
     a = input()
